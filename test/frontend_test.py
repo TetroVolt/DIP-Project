fa4de0a6c571e90b9b14935fc03601756fbd9998
TetroVolt <raymond@sutrisno.me>
2019-04-06 02:21:29 -0500

    added path import

--- test/frontend_test.py
+++ test/frontend_test.py
@@ -1,8 +1,15 @@
+
+import sys; sys.path.append('..');
+
 import unittest
+from frontend import frontend
 
 class TestFrontEnd(unittest.TestCase):
     def test_frontend(self):
         self.assertTrue(True, msg="Is not true.")
+    
+    def test_front_end_import(self):
+        print(dir(frontend))
 
 if __name__=='__main__':
     unittest.main()
