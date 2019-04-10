114795c4410ccf042d942b12394a6050754e1830
TetroVolt <raymond@sutrisno.me>
2019-04-10 13:24:59 -0500

    added open file button

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,14 +1,22 @@
 
 import tkinter as TK
+import tkinter.filedialog
 from tkinter import ttk
 
 # import the Transformations library
 from .Library import TransformFunctions as lib
 
 class App:
-    def __init__(self, master):
+    def __init__(self, master, img=None):
+        self.master = master
         self.tool_box = ttk.Frame(master, padding=20)
         self.tool_box.grid()
+
+        self.openFileButton = ttk.Button(
+            self.tool_box, 
+            text="open file",
+            command=self.openFileButtonPressed)
+        self.openFileButton.grid(column=0)
 
         self.zoomInButton = ttk.Button(
             self.tool_box, 
@@ -34,9 +42,16 @@ class App:
             command=self.rotateButtonPressed)
         self.rotateButton.grid(column=0)
 
-        self.photo = TK.PhotoImage(file='./kennysmall.gif')
+        self.photo = TK.PhotoImage()
         self.photoLabel = TK.Label(master, image=self.photo, padx=80, pady=80)
         self.photoLabel.grid(row=0,column=1)
+
+    def openFileButtonPressed(self):
+        filename =  TK.filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        if filename:
+            self.photo = TK.PhotoImage(file=filename)
+            self.photoLabel = TK.Label(self.master, image=self.photo, padx=80, pady=80)
+            self.photoLabel.grid(row=0,column=1)
 
     def zoomInButtonPressed(self):
         print("Zoom In Button pressed")
@@ -49,4 +64,3 @@ class App:
 
     def rotateButtonPressed(self):
         print("rotate Button pressed")
-
